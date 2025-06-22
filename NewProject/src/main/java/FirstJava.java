d590e0e6bd1b3f3bbc23f33a9f82870da0fea8c6
M.RANILKUMAR <M.RANILKUMAR@Ranil>
2025-06-22 00:12:57 -0400

    New

--- NewProject/src/main/java/FirstJava.java
+++ NewProject/src/main/java/FirstJava.java
@@ -2,10 +2,7 @@
 public class FirstJava {
 
 	public static void main(String[] args) {
-		System.out.println("ABCD EFGH");
-		System.out.println("ABCD EFGH");
-		System.out.println("ABCD EFGH");
-		System.out.println("ABCD EFGH");
-
+		System.out.println("Cham cham");
+		System.out.println("Cham cham 3");
 	}
 }
