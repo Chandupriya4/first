16f84b88db393a778da92884b6f6d264b077cd3d
M.RANILKUMAR <M.RANILKUMAR@Ranil>
2025-06-22 00:08:50 -0400

    cham cham

--- NewProject/src/main/java/FirstJava.java
+++ NewProject/src/main/java/FirstJava.java
@@ -3,5 +3,9 @@ public class FirstJava {
 
 	public static void main(String[] args) {
 		System.out.println("ABCD EFGH");
+		System.out.println("ABCD EFGH");
+		System.out.println("ABCD EFGH");
+		System.out.println("ABCD EFGH");
+
 	}
 }
